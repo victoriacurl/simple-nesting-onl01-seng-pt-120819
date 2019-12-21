7482f201f050a58bc6fe3024428a23af477a8a80
Victoria Curl <victoriacurl9@gmail.com>
2019-12-21 15:53:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,9 @@ def adding_matz
         }
      }
 
-
+programmer_hash[:yukihiro_matsumoto] = {}
+programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+programmer_hash[:yukihiro_matsumoto][:languages] = "Lisp", "C"
 end
 
 def changing_alan
