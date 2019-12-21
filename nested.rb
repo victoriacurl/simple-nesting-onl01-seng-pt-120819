ce3c1e8938b510a6cbc017205ed25ad4ef1d2f5c
Victoria Curl <victoriacurl9@gmail.com>
2019-12-21 15:42:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -19,7 +19,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	programmer_hash[:alan_kay][:known_for]
 
 	programmer_hash =
  		{
@@ -36,6 +35,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
